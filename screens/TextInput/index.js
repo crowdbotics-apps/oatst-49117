8e9df0548445d237dac7319863c197f01b6a7748
Vadym Kolpak <vadym@tuluko.eu>
2024-08-21 02:14:13 +0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/TextInput/index.js
+++ screens/TextInput/index.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { StyleSheet, ScrollView, SafeAreaView, View, Button } from "react-native";
+import { StyleSheet, ScrollView, SafeAreaView, View, Button, TextInput } from "react-native";
 
-const TextInput = () => {
+const TextInputScreen = () => {
   return <SafeAreaView style={styles.safeArea}>
       <ScrollView contentContainerStyle={{
       backgroundColor: "#f0f0f1",
@@ -9,6 +9,7 @@ const TextInput = () => {
       position: "relative",
       flex: 1
     }}>
+        <TextInput style={styles.input} placeholder="Type here..." />
         <View style={styles.buttonContainer}>
           <Button title="Send" onPress={() => {}} />
         </View>
@@ -20,8 +21,14 @@ const styles = StyleSheet.create({
   safeArea: {
     height: "100%"
   },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10
+  },
   buttonContainer: {
     marginTop: 20
   }
 });
-export default TextInput;
+export default TextInputScreen;
