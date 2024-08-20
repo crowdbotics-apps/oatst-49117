50ede4529c301e991a2cab447572050dddc8ba55
Vadym Kolpak <vadym@tuluko.eu>
2024-08-21 02:27:40 +0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/TextInput/index.js
+++ screens/TextInput/index.js
@@ -1,34 +1,37 @@
 import React from "react";
-import { StyleSheet, ScrollView, SafeAreaView, View, Button, TextInput } from "react-native";
+import { StyleSheet, View, Button, TextInput, SafeAreaView, KeyboardAvoidingView, Platform } from "react-native";
 
 const TextInputScreen = () => {
   return <SafeAreaView style={styles.safeArea}>
-      <ScrollView contentContainerStyle={{
-      backgroundColor: "#f0f0f1",
-      padding: 10,
-      position: "relative",
-      flex: 1
-    }}>
-        <TextInput style={styles.input} placeholder="Type here..." />
-        <View style={styles.buttonContainer}>
+      <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={styles.container}>
+        <View style={styles.inputContainer}>
+          <TextInput style={styles.input} placeholder="Type here..." />
           <Button title="Send" onPress={() => {}} />
         </View>
-      </ScrollView>
+      </KeyboardAvoidingView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
-    height: "100%"
+    flex: 1
+  },
+  container: {
+    flex: 1,
+    justifyContent: "flex-end"
+  },
+  inputContainer: {
+    marginBottom: 36,
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 10
   },
   input: {
+    flex: 1,
     height: 40,
     margin: 12,
     borderWidth: 1,
     padding: 10
-  },
-  buttonContainer: {
-    marginTop: 20
   }
 });
 export default TextInputScreen;
